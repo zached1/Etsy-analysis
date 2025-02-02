53e0f6e34b62165fd4b293575d3a2c833985cff4
Zachary Davis <zached1@vt.edu>
2025-02-02 17:46:07 -0500

    Add files via upload

--- Etsy_analysis.R
+++ Etsy_analysis.R
@@ -1,3 +1,4 @@
+library(dplyr) #data manipulation
 
 #First I import the data in, I like file.choose because it always works
 etsy <- read.csv(file.choose())
@@ -5,4 +6,9 @@ etsy <- read.csv(file.choose())
 #this lets us take a look at the data we just imported
 View(etsy)
 
-#we have three quantitative variables, 
+#we have three quantitative variables: cost, review and crowd
+summary(etsy)
+# based on some descriptive statistics, all the reviews seem exceptionally high
+# The average cost of an item is a little less than 20$
+# Average crowd is 537 while the median is 64, clearly not normally distributed
+
