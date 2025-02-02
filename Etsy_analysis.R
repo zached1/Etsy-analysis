706b358c7cd1c7cbd2d3db1081f304ca9452d3d4
Zachary Davis <zached1@vt.edu>
2025-02-02 17:38:24 -0500

    Add files via upload

---
+++ Etsy_analysis.R
@@ -0,0 +1,8 @@
+
+#First I import the data in, I like file.choose because it always works
+etsy <- read.csv(file.choose())
+
+#this lets us take a look at the data we just imported
+View(etsy)
+
+#we have three quantitative variables, 
